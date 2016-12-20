0ae931703d3c7eb02a355f0bf5e58fdd773b72c3
mattfro333 <mattfro333@gmail.com>
2016-12-20 14:56:11 -0700

    Time for Directives.

---
+++ js/timeDirective.js
@@ -0,0 +1,12 @@
+angular.module('timeApp')
+     .directive('showTime', function(){
+      return {
+        retrict: 'E',
+        template: '<div>Good morning {{name}}!</div><p>The current time is {{time}}',
+        link: function(scope, element, attrs){
+          var currentTime = new Date ();
+          scope.time = currentTime.toString();
+
+    }
+   }
+   });
